a594ec7df558e62d1aeee0c2b9bbf6244380c313
stasikmartin <58514490+stasikmartin@users.noreply.github.com>
2020-10-21 17:03:29 +0200

    Add files via upload

---
+++ Data/IPersons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using LoginExample.Data.Impl;
+
+namespace LoginExample.Data
+{
+    public interface IPersons
+    {
+        List<Adult> GetPersons();
+
+        void AddPerson(Adult person);
+
+        void RemovePerson(int id);
+
+        
+    }
+}
